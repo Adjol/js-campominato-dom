40c8795fe9b09ece8705bd48946c8a8254fcbb37
Adjol <ad.mitrushi@gmail.com>
2023-12-06 16:32:14 +0100

    16 random nums gen

---
+++ JavaScript/script.js
@@ -0,0 +1,68 @@
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+//
+
+// let playBtn = document.getElementById("play")
+let griglia = document.querySelector(".grid");
+
+
+
+for (let i=1; i<=100; i++) {
+
+    let quadro = document.createElement("div");
+
+    quadro.className = ("square");
+
+    griglia.append(quadro);
+    
+    quadro.append(i);
+
+    //
+    let cellNums = [];
+    cellNums.push(i);
+    //
+    
+    
+    quadro.addEventListener("click",
+    
+    function(){
+
+        quadro.className = ("clicked_square");
+        console.log(i);
+        
+       
+
+
+    });
+
+
+};
+
+
+
+
+//In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+// Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+
+let casualNums = [];
+for (let j = 0; j <16; j++) {
+    let fire = Math.floor(Math.random() *100) +1;
+    casualNums.push(fire);
+}
+console.log(casualNums);
+
+if (cellNums[i] === casualNums[i]) {
+    quadro.addEventListener("click",
+
+    function(){
+    
+        quadro.className = ("bomb_square");
+        console.log(i);
+    
+    
+    
+    
+    });
+
+};
+
+
